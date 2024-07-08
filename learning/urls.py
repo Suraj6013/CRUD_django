b704e076556da1067918cdf6683114f85beda43a
Suraj6013 <surajwadkar2021.ainds@mmcoe.edu.in>
2024-07-08 21:59:17 +0530

    setting url path

--- learning/urls.py
+++ learning/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import *
 
-urlpatterns = [
+urlpatterns = [    
+    path('' , home , name='home'),-
+    
+
     path('admin/', admin.site.urls),
+
 ]
