0909e99a681d846b116c58cc6d767faf25b80060
Suraj6013 <surajwadkar2021.ainds@mmcoe.edu.in>
2024-07-08 12:47:03 +0530

    feat: Add home app with admin, models, views, tests, and apps configuration

--- learning/settings.py
+++ learning/settings.py
@@ -39,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+EXTERNAL_APPS = [
+    'home',
+]
+
+INSTALLED_APPS += EXTERNAL_APPS
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
